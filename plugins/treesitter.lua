149dc946068f330e32c3823e2706ab34bc96e462
doupongzeng <huangzeng2016@126.com>
2023-06-21 17:01:29 +0800

    feat: add textobject

--- plugins/treesitter.lua
+++ plugins/treesitter.lua
@@ -1,9 +1,89 @@
+local swap_next, swap_prev = (function()
+  local swap_objects = {
+    c = "@class.outer",
+    f = "@function.outer",
+    b = "@block.outer",
+    s = "@statement.outer",
+    a = "@parameter.inner",
+    m = "@call.outer",
+  }
+
+  local n, p = {}, {}
+  for key, obj in pairs(swap_objects) do
+    n[string.format("<leader>s%s", key)] = obj
+    p[string.format("<leader>S%s", key)] = obj
+  end
+
+  return n, p
+end)()
+
 return {
-  "nvim-treesitter/nvim-treesitter",
-  opts = function(_, opts)
-    -- add more things to the ensure_installed table protecting against community packs modifying it
-    opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
-      "cpp", "c"
-    })
-  end,
+  {
+
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      -- add more things to the ensure_installed table protecting against community packs modifying it
+      opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
+        "cpp",
+        "c",
+      })
+    end,
+  },
+  {
+    "nvim-treesitter/nvim-treesitter-textobjects",
+    dependencies = { "nvim-treesitter/nvim-treesitter" },
+    config = function()
+      require("nvim-treesitter.configs").setup {
+        textobjects = {
+          select = {
+            enable = true,
+            lookahead = true,
+            keymaps = {
+              ["ac"] = "@class.outer",
+              ["ic"] = "@class.inner",
+              ["af"] = "@function.outer",
+              ["if"] = "@function.inner",
+              ["ab"] = "@block.outer",
+              ["ib"] = "@block.inner",
+              ["al"] = "@loop.outer",
+              ["il"] = "@loop.inner",
+              ["as"] = "@statement.outer",
+              ["is"] = "@statement.inner",
+              ["aC"] = "@comment.outer",
+              ["iC"] = "@comment.inner",
+              ["am"] = "@call.outer",
+              ["im"] = "@call.inner",
+              ["aa"] = "@parameter.outer",
+              ["ia"] = "@parameter.inner",
+            },
+          },
+          swap = {
+            enable = true,
+            swap_next = swap_next,
+            swap_previous = swap_prev,
+          },
+          move = {
+            enable = true,
+            set_jumps = true, -- whether to set jumps in the jumplist
+            goto_next_start = {
+              ["]m"] = "@function.outer",
+              ["]]"] = "@class.outer",
+            },
+            goto_next_end = {
+              ["]M"] = "@function.outer",
+              ["]["] = "@class.outer",
+            },
+            goto_previous_start = {
+              ["[m"] = "@function.outer",
+              ["[["] = "@class.outer",
+            },
+            goto_previous_end = {
+              ["[M"] = "@function.outer",
+              ["[]"] = "@class.outer",
+            },
+          },
+        },
+      }
+    end,
+  },
 }
